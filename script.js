5874bcca71717081ca6357ff82b6d4fce9d5bf9b
matassalys0 <matassalys@gmail.com>
2022-02-08 21:30:21 +0200

    feat: operation function

--- script.js
+++ script.js
@@ -5,3 +5,29 @@ let subtract = (a, b) => a - b;
 let multiply = (a, b) => a * b;
 let divide = (a, b) => a / b;
 
+function operate(operation) {
+    switch(operation.operator) {
+        case('+'):
+        return add(operation.num1, operation.num2);
+        break;
+        case('-'):
+        return subtract(operation.num1, operation.num2);
+        break;
+        case('*'):
+        return multiply(operation.num1, operation.num2);
+        break;
+        case('/'):
+        return divide(operation.num1, operation.num2);
+        break;
+    }
+}
+
+
+
+let operation = {
+    num1: 5,
+    num2: 10,
+    operator: '*'
+}
+
+console.log(operate(operation));
