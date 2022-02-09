489b7f40bd4a9a77564a0dede2090b890ec3499c
matassalys0 <matassalys@gmail.com>
2022-02-09 22:35:53 +0200

    feat: button inputs showing up on display

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 'use strict'
 
+const inputDiv = document.querySelector('.calc-input');
+
+const operatorArray = ['+', '-', '/', '*'];
 let add = (a, b) => a + b;
 let subtract = (a, b) => a - b;
 let multiply = (a, b) => a * b;
@@ -22,10 +25,30 @@ function operate(operation) {
     }
 }
 
+function numClick(e) {
+    inputDiv.textContent = inputDiv.textContent + e.srcElement.textContent;
+}
+
+function operatorClick(e) {
+    inputDiv.textContent = inputDiv.textContent + e.srcElement.textContent;
+}
+
 let operation = {
     num1: 5,
     num2: 10,
     operator: '*'
 }
+
+const numBtns = document.querySelectorAll('.num-btn');
+numBtns.forEach(button => {
+    button.addEventListener('click', numClick);
+});
+
+const operatorBtns = document.querySelectorAll('.operator-btn');
+operatorBtns.forEach(button => {
+    button.addEventListener('click', operatorClick);
+});
+
+//const equalButton = document.querySelector('.operator-btn');
 
 console.log(operate(operation));
